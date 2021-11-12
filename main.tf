429822785c48a67d24331924cfa623b9c08d937c
RAMARJUN397 <69568813+RAMARJUN397@users.noreply.github.com>
2021-11-12 15:27:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,3 +9,13 @@ provider "azurerm" {
   features {}
 }
 
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "TNetwork" {
+  name                = "myvnet"
+  resource_group_name = "tfdemo"
+  location            = "East US"
+  address_space       = ["10.0.0.0/16"]
+  tags = {
+    environment = "TerraformDemo" 
+  }
+}
