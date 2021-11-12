d7595b0c0ac55c16000bbda87d4578b90c7564ca
RAMARJUN397 <69568813+RAMARJUN397@users.noreply.github.com>
2021-11-12 15:03:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,17 +2,14 @@
 
 
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.84.0"
-    }
-  }
+ 
 }
 
-
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {}  
+  features {}
 }
 
+resource "azurerm_resource_group" "tfDemo" {
+  name     = "Tfreds"
+  location = "West Europe"
+}
