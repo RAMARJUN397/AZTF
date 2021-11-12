03800630db932ee672395e1e9f3e50e2473044ab
RAMARJUN397 <69568813+RAMARJUN397@users.noreply.github.com>
2021-11-12 14:46:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,11 +13,6 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {} 
-  subscription_id = "3d133d92-97d4-4100-96d8-3a6e163da246"
-  client_id    = "e9081202-5020-4a9c-828c-29b0e5e64958"
-  client_secret   = "b6d7abba-5ad5-4c16-bfcb-96c3cacaeb3f"
-  tenant_id       = "14ad6a30-731f-42b0-8013-b85efecc3073"
-  
+  features {}  
 }
 
