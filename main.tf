9131f46b14723ea1bc53d87209d0ffaeb5ecebd3
RAMARJUN397 <69568813+RAMARJUN397@users.noreply.github.com>
2021-11-12 15:38:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,15 +14,8 @@ data "azurerm_client_config" "current" {}
  
 #Create Resource Group
 resource "azurerm_resource_group" "tamops" {
-  name     = "tfremod"
+  name     = "remodemo"
   location = "eastus"
 }
  
-#Create Virtual Network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "tamops-vnet"
-  address_space       = ["192.168.0.0/16"]
-  location            = "eastus2"
-  resource_group_name = "tfremod"
-}
 
